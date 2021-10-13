68e101a5583eeecee780706b5d57572c602ba319
Kavery pm <shiny.kavery@gmail.com>
2021-10-13 10:31:27 -0300

    added css styles

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -4,18 +4,17 @@ import Products from "./pages/products";
 import Welcome from "./pages/welcome";
 function App() {
   return (
-   
     <div>
       <header>
         <MainHeader></MainHeader>
       </header>
       <main>
-      <Route path="/welcome">
-        <Welcome></Welcome>
-      </Route>
-      <Route path="/products">
-        <Products></Products>
-      </Route>
+        <Route path="/welcome">
+          <Welcome></Welcome>
+        </Route>
+        <Route path="/products">
+          <Products></Products>
+        </Route>
       </main>
     </div>
   );
