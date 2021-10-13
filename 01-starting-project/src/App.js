96d68274f3ffe9dbb5683b4c6dadc72458249006
Kavery pm <shiny.kavery@gmail.com>
2021-10-13 10:16:54 -0300

    added routes

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,7 +1,15 @@
+import { Route } from "react-router-dom";
+import Products from "./pages/products";
+import Welcome from "./pages/welcome";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome></Welcome>
+      </Route>
+      <Route path="/products">
+        <Products></Products>
+      </Route>
     </div>
   );
 }
