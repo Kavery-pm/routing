5b7925d09e0ed6feb0d8bfe9f5ab72180951c552
Kavery pm <shiny.kavery@gmail.com>
2021-10-14 02:39:28 -0300

    added nested routes

--- 01-starting-project/src/pages/welcome.js
+++ 01-starting-project/src/pages/welcome.js
@@ -1,4 +1,13 @@
-const Welcome = ()=>{
-    return <h1>WELCOME </h1>
-}
+import { Route } from "react-router";
+
+const Welcome = () => {
+  return (
+    <section>
+      <h1>Hello</h1>
+      <Route path="/welcome/new-users">
+        <h1>WELCOME </h1>
+      </Route>
+    </section>
+  );
+};
 export default Welcome;
